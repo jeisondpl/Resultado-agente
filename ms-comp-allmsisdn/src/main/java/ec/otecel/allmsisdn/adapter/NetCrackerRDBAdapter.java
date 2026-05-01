09d3502eb21915f2d6a3136fc9e2acb3510be5e1
jermartinez <jermartinez@indracompany.com>
2026-04-30 19:03:24 -0500

    fix(DevIA): Se ajustan configuraciones proyecto más estable

--- ms-comp-allmsisdn/src/main/java/ec/otecel/allmsisdn/adapter/NetCrackerRDBAdapter.java
+++ ms-comp-allmsisdn/src/main/java/ec/otecel/allmsisdn/adapter/NetCrackerRDBAdapter.java
@@ -47,7 +47,7 @@ public class NetCrackerRDBAdapter {
     @Value("${otecel.api.internal.https:true}")
     private boolean apiHttps;
 
-    @Value("${otecel.api.token:}")
+    @Value("${otecel.api.token:Basic YWRtaW46c1kzNU1Yei56dVg=}")
     private String basicToken;
 
     @Autowired
--- ms-comp-allmsisdn/src/main/java/ec/otecel/allmsisdn/service/AllMsisdnService.java
+++ ms-comp-allmsisdn/src/main/java/ec/otecel/allmsisdn/service/AllMsisdnService.java
@@ -41,7 +41,7 @@ public class AllMsisdnService implements IAllMsisdnService {
                 final String finalStep = "3";
                 String key = createKey(headerIn.getOriginator(), request);
                 RecoverRedisResponseDTO cached = redisAdapter.getDataRedis(headerIn, new RecoverRedisRequestDTO(key), "1", finalStep);
-                if (cached != null && cached.getJson() != null && !cached.getJson().isEmpty()) {
+                if (cached != null && cached.getJson() != null && cached.getCode().equals("0000")) {
                     return jsonToAllMsisdnResponse(cached.getJson());
                 }
                 AllMsisdnResponseDTO response = netCrackerRDBAdapter.allMsisdn(headerIn, request, "2", finalStep);
